494cec09c374b60f1dc2ddc04f6e6c34e7819f49
Catherine Lin <cathy8295@gmail.com>
2018-09-16 21:47:29 -0400

    Added homepage and teh start of css

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Hello World</title>
+  <meta charset='utf-8' >
+
+
+  <link rel='/css/main.css' rel='stylesheet'>
+
+</head>
+
+<body>
+  <h1>Hello World</h1>
+  <img src='image/dog.jpg'>
+</body>
+</html>
