8af27955677feaec3256747f49ed83daa8d9f61e
Catherine Lin <cathy8295@gmail.com>
2018-09-18 18:22:42 -0400

    Add images and quotes

--- index.php
+++ index.php
@@ -5,12 +5,28 @@
   <meta charset='utf-8' >
 
 
-  <link rel='/css/main.css' rel='stylesheet'>
+  <link href='css/main.css' rel='stylesheet'>
 
 </head>
 
 <body>
-  <h1>Hello World</h1>
+  <h1>About me</h1>
   <img src='images/dog.jpg'>
+  <p>
+
+  A software developer with skills of C#, Java, HTML, CSS.
+
+  </p>
+
+  <?php
+$a=array("Imagination is more important than knowledge.-Einstein",
+"Life is like riding a bicycle. To keep your balance you must keep moving.-Einstein",
+"Look deep into nature, and then you will understand everything better.-Einstein",
+"No problem can be solved from the same level of consciousness that created it.-Einstein",
+"I have no special talents. I am only passionately curious.-Einstein");
+$random_keys=(rand(0,4));
+echo $a[$random_keys]."<br>";
+
+?>
 </body>
 </html>
